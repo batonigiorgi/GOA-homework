bf3853d17c916dc6fcb883afaa87f38a19575dae
batonigiorgi <gmindorashvili0@gmail.com>
2024-04-30 22:17:03 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,97 @@
+from turtle import *
+#we want to paint a house
+#step 1 draw a square
+speed(30)
+width(7)
+color("purple")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+begin_fill()
+forward(70)
+color("yellow")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right (90)
+forward(120)
+end_fill()
+color("yellow")
+
+ 
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+color("purple")
+#making windows
+penup()
+goto(55, 125)
+left(30)
+right(90)
+pendown()
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(20)
+right(90)
+forward(40)
+left(90)
+forward(20)
+left(90)
+forward(20)
+left(90)
+forward(40)
+#making right window of house
+penup()
+goto(145, 125)
+pendown()
+
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(990)
+right(990)
+left(90)
+forward(20)
+left(90)
+forward(40)
+left(90)
+forward(20)
+left(90)
+forward(20)
+left(90)
+forward(40)
+
+
+
+
+
+
+
+exitonclick()
