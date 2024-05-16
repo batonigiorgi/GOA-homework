27454d81df9c37abd3ba9d28a4f644d53eebb0c2
batonigiorgi <gmindorashvili0@gmail.com>
2024-05-16 21:30:44 +0400

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,16 @@
+print(5 + 5)
+#maghla mocemul kodshi chven davamatet ertmanets ori ricxvi risi pasuxic terminalshi sworad gamovuida
+print(5 - 5)
+#maghla mocemul kodshi chven gamovakelit ertmanets ori ricxvi
+print(5 * 5)
+#maghla mocemul koshi chven gavamravlet ori ricxvi
+print(5 / 5)
+#maghla mocemul kodshiu chven gavyavit ertamnetze ori ricxvi
+#print(5+ batonigiorgi) if i run terminal it will give me eror bc of this code
+print("batoni" + "giorgi")
+#when i add batoni on giorgi terminal is giving me batonigiorgi
+#terminali gamoiyeneba imistvis ,rom chven vnaxot chveni dawerili kodis shedegi 
+x=(9)
+y=(2)
+print(x + y)
+#stringi aris or ormag brchyal shoris moqceuli monacemta tipi ("") .xolo mteli aris or ertmag brchyals shoris mocemuli monacemta tipi (')
