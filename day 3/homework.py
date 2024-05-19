44695c4b3cbcd4c77db2651052fa128768b9d931
batonigiorgi <gmindorashvili0@gmail.com>
2024-05-19 22:41:36 +0400

    third commit

---
+++ day 3/homework.py
@@ -0,0 +1,53 @@
+#DAVALEBA 1
+#nivtis fasi
+price=10
+#nivtis odenoba
+amount=5
+#ramdens gadaixdis clienti 5ive nivtis shedzenis shemtxvevashi
+print(price * amount)
+
+
+#DAVALEBA 2
+#chemi asaki
+age1=15
+#chemi ojaxiswevris asaki
+age2=25
+#chem asaks +10
+print(age1 + 10)
+#chemi ojaxiswevris asaks +10
+print(age2 + 10)
+
+
+#DAVALEBA 3
+#cvladi pirveli
+x=5
+#cvladi meore
+y=10
+#mimateba 
+print(x + y)
+#gmaokleba
+print(x - y)
+#gamravleba
+print(x * y)
+#gayofa
+print(x / y)
+
+
+#DAVALEBA 4
+x="giorgi"
+y=15
+print(x)
+print(y)
+
+
+#DAVALEBA 5
+#chemi asaki
+x= 15
+#chemi mshoblis asaki
+y= 50
+#chemi asaki 10 welshi
+z= x + 10
+#chemi mshoblis asski 10 welshi
+d= y + 10
+#ramdenjer didi iqneba chemi mshobeli chemze 10 welshi
+print(d / z)
