fc148a766e4d5a1e75ec8cd63836ccea7af1f075
batonigiorgi <gmindorashvili0@gmail.com>
2024-05-26 21:42:59 +0400

    fourth commit

---
+++ day 4/homework.py
@@ -0,0 +1,50 @@
+#1
+
+firstname=input('Please enter your firstname: ')
+print(firstname)
+
+
+lastname=input('please enter your lastname: ')
+print(lastname)
+
+print(firstname + " " + lastname)
+
+#2
+
+x=(15)
+
+y=int(input('please enter number:  '))
+
+print(x + y)
+
+#3
+
+x=(2)
+y=(3)
+z=(7)
+print((x + y + z)/3 )
+
+#4
+#momxmarebels shemovataninot mis surmilisdamebr ricxvi
+number=int(input("enter number: "))
+#gavamravlot 5ze momxmareblis mier shercheuli ricxvi
+print(number * 5)
+#shemovitanopt cvladi da ricxvi
+x=(5)
+#5ze gavamravlot chvens mier shemotanili ricxvi
+print(x * 5)
+
+
+#5
+
+num1=float(input('please enter your number:  '))
+num2=float(input("please enter you number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+
+
+
