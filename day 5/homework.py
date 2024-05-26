9f49d4213e026f5c831c6cf192282a05438700a1
batonigiorgi <gmindorashvili0@gmail.com>
2024-05-26 23:16:10 +0400

    fifth commit

---
+++ day 5/homework.py
@@ -0,0 +1,51 @@
+#int funqciis gamoyeneba
+print(int(5.5 + 5))
+print(int(7.8 + 9))
+print(int(9.9 + 14))
+print(int(4.7 + 15))
+#print(int(10 + "4"))
+
+#float funqciis gamoyeneba
+
+print(float(13 / 5))
+print(float(15 / 5))
+print(float(10 / 20))
+print(float(10 / 5))
+print(float("10" / 5))
+
+
+#string funqciis gamoyeneba
+
+print(str("5.6" + "giorgi"))
+print(str(5 * 5))
+print(str(1 +1))
+print(str(5 * "giorgi"))
+#print(str("5" * giorgi))
+
+
+
+
+#2
+#momxamrebels shemovataninet ricxvi num1
+num1=float(input('please enter number: '))
+#num2\
+num2=float(input("please enter number: "))
+#num3
+num3=float(input("please enter number: "))
+#num4
+num4=float(input("please enter number: "))
+#num5
+num5=float(input("please enter number: "))
+
+#mivumatot ertmanets 
+print(num1 + num2 + num3 + num4 + num5)
+#gamovaklot ertmanets
+print(num1 - num2 - num3 - num4 - num5)
+#gavamravlot ertmanetze
+print(num1 * num2 * num3 * num4 * num5)
+#gavyot ertmanetze
+print(num1 / num2 / num3 / num4 / num5)
+#vabatilebt ertmanets
+print(num1 // num2 // num3 // num4 // num5)
+
+print(num1 % num2 % num3 % num4 % num5)
