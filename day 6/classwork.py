7da36ee520a539f0d005dc9ee86e54644a6a6665
batonigiorgi <gmindorashvili0@gmail.com>
2024-05-28 20:17:39 +0400

    sixth commit

---
+++ day 6/classwork.py
@@ -0,0 +1,14 @@
+#5 true mnishvneloba
+print(5 == 5)
+print(5 > 2)
+print(25 >= 7)
+print(40 <= 50)
+
+#5 false mnishvneloba
+
+print(5 == 6)
+print(5 > 7)
+print(6 < 2)
+print(6 >= 8)
+print(9 <= 5)
+
---
+++ day 6/homewok.py
@@ -0,0 +1,67 @@
+# metobis nishani >
+
+print(5 > 3)
+print(5 > 4)
+print(3 > 99)
+print(99 > 4)
+print(0 > 45)
+
+# naklebobis nishani <
+
+print(3 < 5)
+print(8 < 3)
+print(0 < 3)
+print(399 < 2)
+print(0 < 2)
+
+#tolobis nishani ==
+
+print(5 == 5)
+print(4 == 5)
+print(3 == 3)
+print(2 == 99)
+print(4 == 4)
+
+# metia an tolias nishani >=
+
+print(5 >= 2)
+print(5 >= 7)
+print(2 >= 2)
+print(99 >= 7)
+print(0 >= 2)
+
+# naklebia an tolias nishani <=
+
+print(2 <= 4)
+print(8 <= 6)
+print(1 <= 99)
+print(9 <= 8)
+print(0 <= 0)
+
+# ar udris nishani !=
+
+print(3 != 4)
+print(6 != 6)
+print(3 != 3)
+print(8 != 8)
+print(3 != 99)
+
+
+
+# 2
+
+#and
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(5 > 8 and 8 < 5)
+print(5 >= 8 and 8 < 5)
+
+#or
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print(9 > 8 or 7 < 5)
+print(3 >= 8 or 4 < 5)
