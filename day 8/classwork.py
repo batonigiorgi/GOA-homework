2454c0464163675a4b0ab9860c5513118bcb4d02
batonigiorgi <gmindorashvili0@gmail.com>
2024-06-03 16:57:20 +0400

    eighth commit

---
+++ day 8/classwork.py
@@ -0,0 +1,18 @@
+#for i in range(10):
+   # print("Luka")
+
+for i in range(20):
+   print("Giorgi")
+
+
+num1=int(input('Enter your number'))
+num2=int(input('enter your number 2: '))
+
+
+
+if num1 > num2:
+   print(num1)
+else:
+    print(num2)
+
+
---
+++ homework.py
@@ -0,0 +1,51 @@
+#1
+
+for i in range(21):
+    print(i) #0 dan 20 chatvlit ricxvebi
+
+#2
+
+for i in range(10):
+    print(i) #priveli 10 natularuli ricxvi
+
+#3
+
+for i in range(0, 101, 2):
+    print(i)
+
+#4
+
+num1 = float(input("Enter Your Number: "))
+if num1 > 0:
+    print("Number is positive")
+if  num1 < 0:
+    print("Number is negative")
+if num1 == 0:
+    print("Number is zero")
+
+#5
+
+age= int(input("Enter your age: "))
+if age > 18: 
+    print("You are virgin")
+if age < 18:
+    print("You are adult")
+
+#6
+week = int(input("enter number of the day in week:"))  
+if week == 1:
+    print("Monday")  
+elif week == 2:
+    print("Tuesday")
+elif week == 3:
+    print("Wednesday")
+elif week == 4:
+    print("Thursday")
+elif week == 5:
+    print("Friday")
+elif week == 6:
+    print("Saturday")
+elif week == 7:
+    print("Sunday")
+else:
+    print("ERROR")
