cf3e14a2cf04819d2432a36143591f7fa8b7081e
batonigiorgi <gmindorashvili0@gmail.com>
2024-06-22 20:49:21 +0400

    eightteenth commit

---
+++ day 18/homework/homework.py
@@ -0,0 +1,8 @@
+name_list = ["Giorgi", "Nika"]
+
+
+for name in name_list:
+    if name == "Giorgi":
+        print("hello admin")
+    else:
+        print("hello user")
---
+++ day 18/homework/homework2.py
@@ -0,0 +1,10 @@
+name_list = ["Nika", "Giorgi", "Sandro"]
+
+name = (input("Please Enter Your Name: "))
+
+for name_list in name:
+    if name == "Giorgi":
+        print("hello admin")
+    else:
+        print("hello user")
+
