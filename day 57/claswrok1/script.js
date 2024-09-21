4ee9c5f963a0cc6a0f1a84e4c96b037036942ce0
batonigiorgi <gmindorashvili0@gmail.com>
2024-09-21 13:27:34 +0400

    Groupwork commit

---
+++ day 57/claswrok1/script.js
@@ -0,0 +1,57 @@
+//-------------1------------
+
+const PI = 3.14;
+console.log(PI); 
+PI = 3.14159;  //კონსტანტის მნიშვნელობის შეცვლა არ შეილება
+
+//------------2------------
+
+let age = 25;
+console.log(age);
+age = 30;
+console.log(age); 
+
+
+//------------3-----------
+
+let x = 5;
+let y = 10;
+x = x + y; 
+y = x - y;
+x = x - y; 
+console.log(x, y);
+
+
+//--------------4------------
+
+const favoriteColor = "black";
+console.log(favoriteColor);
+favoriteColor = "red";  //კონსტანტის მნიშვნელობის შეცვლა არ შეილებბა
+
+
+//------------5--------------
+
+let a = 10;
+let b = 5;
+
+console.log(a + b);  
+console.log(a - b);  
+console.log(a * b);  
+console.log(a / b); 
+console.log(a % b); 
+
+//---------------6-------------
+
+let result = 2 ** 5;
+console.log(result);
+
+//-------------7--------------
+
+let width = 5;
+let height = 2;
+let s = width * height;
+console.log(s);
+
+//-----------------8-------------
+
+
